278c4a7d616851031d039271fa6f848e8ee2e393
FelineFantasy <mailsalavata5@gmail.com>
2026-08-11 15:31:26 +0500

    Revert "Merge pull request #1 from FelineFantasy/size"
    This reverts commit 6408e0611ddeeb68df0c52a0b12386435a5702a2, reversing
    changes made to 1080723e82b49122ffa43def6084b2a98f14d02f.

--- hello.asm
+++ hello.asm
@@ -6,12 +6,12 @@ section .text
     global _start
 
 _start:
-    mov eax, 1
-    mov edi, 1
-    mov esi, msg
-    mov edx, len
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, msg
+    mov rdx, len
     syscall
 
-    mov eax, 60
-    xor edi, edi
+    mov rax, 60
+    mov rdi, 0
     syscall
